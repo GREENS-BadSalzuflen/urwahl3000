b1fd4260b65c6e62dd41587bac9ec3fda823db8e
Sebastian Klatte <sebastianklatte@gmx.net>
2016-12-16 00:36:48 +0100

    Fix posts and event posts shown together in archive list.

--- functions/theme-custom-functions.php
+++ functions/theme-custom-functions.php
@@ -7,8 +7,7 @@
 * @return WP_Query
 */
 function add_custom_post_type( $query ) {
-    if ( $query->is_main_query() && is_home() )
-		
+    if ( $query->is_main_query() && ( is_home() || is_archive() || is_category() ) )
         $query->set( 'post_type', array( 'post', 'ai1ec_event' ) );
     return $query;
 }
