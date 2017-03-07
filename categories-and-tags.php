3f398eda32aee18ff5d6cd79de0b1be309e46a2e
Sebastian Klatte <sebastianklatte@gmx.net>
2017-03-07 11:14:40 +0100

    Fix tags from event categories.

--- categories-and-tags.php
+++ categories-and-tags.php
@@ -10,9 +10,17 @@
 												}
 												echo $category_list;
 												
-												$tag_list = get_the_tag_list( '<i class="fa fa-tag" aria-hidden="true"></i> ', ', ', '' );
+												$tag_list = '';
+												if ( 'post' == get_post_type() ) {
+													$tag_list = get_the_tag_list( '<i class="fa fa-tag" aria-hidden="true"></i> ', ', ', '' );
+												}
+												elseif ( 'ai1ec_event' == get_post_type() ) {
+													$tag_list = get_the_term_list( '', 'events_tags', '<i class="fa fa-tag" aria-hidden="true"></i> ', ', ', '' );
+												}
+												
 												if ( '' != $category_list && '' != $tag_list )
 													echo ' ';
+												
 												echo $tag_list;
 												
 												?></p>
